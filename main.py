374fe9f3eb83324896db2ec0ca319ffc0652d3fa
chayane-kha <chayane.kha@edu.esiee.fr>
2024-11-08 13:16:26 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
+"""import de la fonction sqrt du module math"""
 from math import sqrt
 
 #### Fonction secondaire
 
 
 def isprime(p):
+    """
+    Retourne True si p est premier sinon retourne False
 
+    """
     # votre code ici
-
-    pass
+    if p == 1:
+        return False
+    for i in range(2,(int)(sqrt(p)+1)):
+        if p%i == 0:
+            return False
+    return True
 
 #### Fonction principale
 
 
 def main():
+    """
+    Programme principal
+    """
 
     # vos appels à la fonction secondaire ici
 
